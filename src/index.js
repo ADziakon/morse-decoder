3068d3eaca2b56ede718e11f5b1b48e41e7aff85
ADziakon <a.dziakanchuk@gmail.com>
2023-02-22 14:38:00 +0300

    feat: add task morse

--- src/index.js
+++ src/index.js
@@ -39,6 +39,70 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    str1 = "";
+str2 = "";
+str3 = "";
+str = "";
+res = "";
+res1 = "";
+arr = [];
+arr2 = [];
+arr3 = [];
+arrayOfStrings = [];
+
+for (i = 0; i < expr.length; i++) {
+  str += expr[i];
+  if (str.length == 10) {
+    arrayOfStrings.push(str);
+    str = "";
+  }
+}
+
+
+for (i = 0; i < arrayOfStrings.length; i++) {
+  for (j = 0; j < 10; j++) {
+    if (arrayOfStrings[i][0] == 0) {
+      arrayOfStrings[i] = arrayOfStrings[i].slice(1);
+    }
+    if (arrayOfStrings[i][0] == 1) {
+      break;
+    }
+  }
+}
+
+for (i = 0; i < arrayOfStrings.length; i++) {
+
+str1 = '';
+  for (j = 0; j < arrayOfStrings[i].length; j += 2) {
+
+    str2 = arrayOfStrings[i][j] + arrayOfStrings[i][j + 1];
+    
+    if (str2 == '10'){
+        str1 += '.';
+        
+    }
+    if(str2 == '11'){
+        str1 += '-';
+    }
+    str2='';
+  }
+  arr.push(str1);
+}
+
+for(i=0; i<arr.length;i++){
+    if(arr[i].length == 0){
+        arr3.push(' ');
+        continue;
+    }
+        for (key in MORSE_TABLE){
+            if (arr[i] == key){
+                arr3.push(Object.values(MORSE_TABLE[key]));
+            }
+        } 
+    
+}
+
+    return  arr3.join('');
 }
 
 module.exports = {
